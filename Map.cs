854574ad07131518e4238f20ce0370040c641f7c
pppppengJ <153730553+pppppengJ@users.noreply.github.com>
2026-05-27 17:52:08 +0900

    Add files via upload

---
+++ Map.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeepEchoGame
+{
+    class Map
+    {
+        private List<Zone> Zones = new List<Zone>(); //모든 구역을 리스트에 저장
+
+        public Map() //게임 시작 시 맵 생성
+        {
+            Zone cam1 = new Zone("cam1"); //앞쪽                ㅡ 1 ㅡㄱ
+            Zone cam2 = new Zone("cam2"); //오른쪽            |    |    |
+            Zone cam3 = new Zone("cam3"); //왼쪽              3 ㅡ 5 ㅡ 2  이런 식으로 연결?
+            Zone cam4 = new Zone("cam4"); //뒷쪽              |    |    |
+            Zone cam5 = new Zone("cam5"); //위쪽              ㄴㅡ 4 ㅡ 
+
+            cam1.Connect(cam2);
+            cam1.Connect(cam3);
+            cam1.Connect(cam5);
+
+            cam2.Connect(cam1);
+            cam2.Connect(cam4);
+            cam2.Connect(cam5);
+
+            cam3.Connect(cam1);
+            cam3.Connect(cam4);
+            cam3.Connect(cam5);
+
+            cam4.Connect(cam2);
+            cam4.Connect(cam3);
+            cam4.Connect(cam5);
+
+            cam5.Connect(cam1);
+            cam5.Connect(cam2);
+            cam5.Connect(cam3);
+            cam5.Connect(cam4);
+
+            Zones.Add(cam1);
+            Zones.Add(cam2);
+            Zones.Add(cam3);
+            Zones.Add(cam4);
+            Zones.Add(cam5);
+        }
+
+
+        //Zone에 접근
+        public void MonsterIn(int index) //cam[번호]에 몬스터 생성
+        {
+            if (index > 0 && index <= Zones.Count)
+                Zones[index - 1].MonsterIn();
+        }
+
+        public void MonsterOut(int index) //cam[번호]에 몬스터 삭제
+        {
+            if (index > 0 && index <= Zones.Count)
+                Zones[index - 1].MonsterOut();
+        }
+
+        public Zone FindZone(int index) //scan할 때 작동
+        {
+            if (index > 0 && index <= Zones.Count)
+                return Zones[index - 1];
+
+            return null;
+        }
+    }
+}
---
+++ Zone.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeepEchoGame
+{
+    class Zone
+    {
+        private string name;
+        private List<Zone> connectedZones;
+        private bool hasMonster;
+
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+        public List<Zone> ConnectedZones
+        {
+            get { return connectedZones; }
+            private set { connectedZones = value; }
+        }
+        public bool HasMonster
+        {
+            get { return hasMonster; }
+            private set { hasMonster = value; }
+        }
+
+        public Zone(string name) //생성 시 zone 정의
+        {
+            this.Name = name;
+            this.ConnectedZones = new List<Zone>();
+            this.HasMonster = false;
+        }
+
+        public void Connect(Zone name) //생성 시 zone 연결
+        {
+            ConnectedZones.Add(name);
+        }
+
+        public void MonsterIn() //zone에 몬스터 생김
+        {
+            HasMonster = true;
+        }
+
+        public void MonsterOut() //zone에 몬스터 사라짐
+        {
+            HasMonster = false;
+        }
+    }
+}
